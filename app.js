e3feae4cf0b4c63d74bc0def1ddd2ff31c6b9eda
Manickam T <163423524+manickam1024@users.noreply.github.com>
2024-09-10 08:50:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'; // React 18+ uses 'react-dom/client'
 
 
 
-const ele1=React.createElement("h2",{},"hello world!");
+const ele1=React.createElement("h2",{},"hello world! mani");
 
 const ele=React.createElement("h1",{},"hello world!");
 const child=React.createElement("div",{id:"child"},ele,ele1);

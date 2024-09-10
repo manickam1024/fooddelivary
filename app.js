6ea2e961006db39e07d0e4ec83426a7c04f5fe6a
manickam1024 <manicyam1234@gmail.com>
2024-09-10 13:41:09 +0530

    component compostion

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client' // React 18+ uses 'react-dom/client'
 
-const Parent = () => (
-  <h1 className="msg" tabIndex={2}>
-    hello world
-  </h1>
+const Title = <h1>hello world </h1>
+
+const Body = () => (
+  <div>
+    {Title}
+    <h1>this is component composition</h1>
+  </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Parent />)
+root.render(<Body />)
